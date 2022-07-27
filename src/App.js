e73c64938be359b6eabd5c878e1ac8294f10c724
pallavi <pallavi.ds@trempplin.com>
2022-07-27 15:24:34 +0530

    completed

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import React,{useState} from 'react'
+import './index.css'
+
+
+function App() {
+  //state
+  const [weight, setWeight] = useState(0)
+  const [height, setHeight] = useState(0)
+  const [bmi, setBmi] = useState('')
+  const [message, setMessage] = useState('')
+
+ 
+
+  let calcBmi = (event) => {
+    //prevent submitting 
+    event.preventDefault()
+    
+
+    if(weight === 0 || height === 0){
+      alert('please enter a valid height and weight')
+
+    }else {
+      let bmi = (weight / (height * height) * 703)
+      setBmi(bmi.toFixed(1))
+    
+    if (bmi < 25) {
+      setMessage('You are underweight')
+    } else if (bmi >= 25 && bmi < 30) {
+      setMessage('You are a healthy weight')
+    } else {
+      setMessage('You are overweight')
+    }
+  }
+
+  }
+
+  let imgSrc='';
+  if (bmi < 1) {
+    imgSrc = null
+  } else {
+    if(bmi < 25) {
+      imgSrc = require('../src/images/underweight.jpg')
+    } else if (bmi >= 25 && bmi < 30) {
+      imgSrc = require('../src/images/healthyweight.jpg')
+    } else {
+      imgSrc = require('../src/images/overweight.jpg')
+    }
+  }
+
+  let reload = () => {
+    window.location.reload()
+  }
+
+  
+  return (
+    <div className="app">
+      <div className='container'>
+      <h2 className='center'>BMI CALCULATOR</h2>
+      <form onSubmit={calcBmi}>
+
+        <div>
+          <label>weight(lbs)</label>
+          <input value={weight} onChange={(e) => setWeight(e.target.value)}/>
+        </div>
+
+        <div>
+          <label>height (in)</label>
+          <input value={height} onChange={(event) => setHeight(event.target.value)}/>
+        </div>
+
+        <div>
+          <button className='btn' type='submit'>Submit</button>
+          <button className='btn btn-outline'onClick={reload} type='submit'>Reload</button>
+        </div>
+      </form>
+
+      <div className='center'>
+      <h3> your BMI is: {bmi}</h3> 
+    
+      <p>{message}</p>
+      </div>
+
+    <div className='img-container'>
+      <img src={imgSrc} alt=''></img>
+     </div>
+    
+
+    </div>
+    </div>
+    
+  );
+  
+}
+
+export default App;
